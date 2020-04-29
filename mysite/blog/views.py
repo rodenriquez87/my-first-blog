c53779342be3cb46e8c0ede1642096d44065e7de
rodenriquez <rodenriquez87@gmail.com>
2020-04-28 23:10:10 -0500

    Actualizar vistas y plantilla para el detalle del post del blog asi como tambien CSS para el sitio.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    # return render(request, 'blog/post_detail.html',{})
-    return render(request, 'blog/post_detail.html', {'posts': posts})
+    # return render(request, 'blog/post_list.html',{})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
